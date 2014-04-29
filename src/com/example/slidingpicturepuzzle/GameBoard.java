3576ce3cb099241dc924be51501615096424aa50
Chris Dunn <christopherdunn711@gmail.com>
2014-04-29 17:05:02 -0500

    working on gameboard and moving imgs in test project

--- src/com/example/slidingpicturepuzzle/GameBoard.java
+++ src/com/example/slidingpicturepuzzle/GameBoard.java
@@ -1,5 +1,13 @@
 package com.example.slidingpicturepuzzle;
 
+import android.graphics.drawable.Drawable;
+
 public class GameBoard {
+	private GamePiece mBoard[][];
+	private boolean mIsComplete;
+	
+	public GameBoard(int size, Drawable img){
+		mIsComplete = false;
+	}
 
 }
