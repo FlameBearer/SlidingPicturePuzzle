a78b4a7467b7a17692d61e3e81f317471069170a
Chris Dunn <christopherdunn711@gmail.com>
2014-04-28 23:37:00 -0500

    add more images and arrays.xml

--- gen/com/example/slidingpicturepuzzle/R.java
+++ gen/com/example/slidingpicturepuzzle/R.java
@@ -8,6 +8,9 @@
 package com.example.slidingpicturepuzzle;
 
 public final class R {
+    public static final class array {
+        public static final int default_image_options=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,30 +20,33 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int bulbasaur_large=0x7f020000;
-        public static final int charmander_large=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int squirtle_large=0x7f020003;
+        public static final int beef_fajita=0x7f020000;
+        public static final int bulbasaur_large=0x7f020001;
+        public static final int charmander_large=0x7f020002;
+        public static final int dirty_sanchez=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int squirtle_large=0x7f020005;
+        public static final int taco_truck_logo=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int imageSelection=0x7f080000;
+        public static final int action_settings=0x7f090001;
+        public static final int imageSelection=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int addDelete=0x7f050003;
-        public static final int app_name=0x7f050000;
-        public static final int selection=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int addDelete=0x7f060003;
+        public static final int app_name=0x7f060000;
+        public static final int selection=0x7f060002;
     }
     public static final class style {
         /** 
@@ -64,11 +70,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int mainTextView=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int mainTextView=0x7f070002;
     }
 }
