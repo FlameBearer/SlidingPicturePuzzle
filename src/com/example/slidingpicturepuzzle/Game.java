844e7206de9da555dfc969522e9d4fd673c75e9a
Chris Dunn <christopherdunn711@gmail.com>
2014-05-01 15:30:44 -0500

    added buttons to main screen instead of using menu

--- src/com/example/slidingpicturepuzzle/Game.java
+++ src/com/example/slidingpicturepuzzle/Game.java
@@ -1,5 +1,28 @@
 package com.example.slidingpicturepuzzle;
 
-public class Game {
+import android.app.Activity;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.GridView;
+
+public class Game extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board_layout);
+        
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
