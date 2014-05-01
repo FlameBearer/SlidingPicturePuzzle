f581031b007e15e666735ba8c08687d7028385ad
Chris Dunn <christopherdunn711@gmail.com>
2014-05-01 09:35:51 -0500

    added moving setting

--- src/com/example/slidingpicturepuzzle/GamePiece.java
+++ src/com/example/slidingpicturepuzzle/GamePiece.java
@@ -5,10 +5,17 @@ import android.graphics.drawable.Drawable;
 public class GamePiece {
 	private Drawable mImg;
 	private final int mNumber;
+	private int mMobility;
+	public static final int NONE = 0;
+	public static final int LEFT = 1;
+	public static final int RIGHT = 2;
+	public static final int UP = 3;
+	public static final int DOWN = 4;
 	
 	public GamePiece(Drawable img, int number){
 		mImg = img;
-		mNumber = number; 
+		mNumber = number;
+		mMobility = NONE;
 	}
 	
 	public int getNumber(){
